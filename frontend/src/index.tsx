a36978d4c3f9ff5c7390f1f92d82ae065ea5a86f
johnsonejoga456 <ejogajohnson@gmail.com>
2025-02-03 23:26:19 +0100

    I don't know what I'm pushing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TodoPage from './pages/Todopage';
+import TodoPage from './pages/TodoPage';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
